6abad44f6c445c8d6e41b701b08942a3d451ab62
b12pr <b12pr@LAPTOP-2VPNMKCO.mshome.net>
2022-02-09 17:16:21 +0530

    created new class product

---
+++ productmanagement/src/productmanagement/Product.java
@@ -0,0 +1,15 @@
+package productmanagement;
+
+public class Product {
+
+	private Integer pid;
+
+	public Integer getPid() {
+		return pid;
+	}
+
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+	
+}
